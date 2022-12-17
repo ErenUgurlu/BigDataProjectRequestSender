a99157a5200a0d35292cd2ca29f316fe1882396b
ErenUgurlu <erentrugurlu@gmail.com>
2022-12-17 15:29:52 +0300

    ilk commit bos proje

---
+++ src/main/java/com/bigdata/sender/Request/Sender/RequestSenderApplication.java
@@ -0,0 +1,13 @@
+package com.bigdata.sender.Request.Sender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestSenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestSenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bigdata/sender/Request/Sender/RequestSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigdata.sender.Request.Sender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestSenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
